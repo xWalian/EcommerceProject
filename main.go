e91482c18d1611ada0bb20f61756d665322b62bf
xWalian <lukasek008@gmail.com>
2024-03-04 15:20:05 +0100

    setting up

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Oops", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(rw, "Hello %s", d)
+
+	})
+	http.HandleFunc("/users", func(http.ResponseWriter, *http.Request) {
+		log.Println("Hello Earth")
+	})
+	http.ListenAndServe(":8080", nil)
+}
