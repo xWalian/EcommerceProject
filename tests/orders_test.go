828617064a572074191aef102a37f66b7a35b241
xWalian <lukasek008@gmail.com>
2024-03-06 09:50:26 +0100

    Added products and orders services and configured proto files for grpc

---
+++ tests/orders_test.go
@@ -0,0 +1,88 @@
+// orders_test.go w folderze tests
+
+package tests
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/xWalian/EcommerceProject/microservices/orders"
+)
+
+func TestCreateOrder(t *testing.T) {
+	// Utwórz nowego klienta MongoDB w pamięci.
+	db, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	assert.NoError(t, err)
+	defer db.Disconnect(context.Background())
+
+	// Utwórz nowy serwer zamówień z podłączoną bazą danych MongoDB.
+	server := orders.NewServer(db)
+
+	// Wywołaj funkcję CreateOrder.
+	order, err := server.CreateOrder(context.Background(), &orders.CreateOrderRequest{
+		UserId:     "user123",
+		ProductIds: []string{"product1", "product2"},
+	})
+	assert.NoError(t, err)
+	assert.NotNil(t, order)
+}
+
+func TestGetOrder(t *testing.T) {
+	// Utwórz nowego klienta MongoDB w pamięci.
+	db, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	assert.NoError(t, err)
+	defer db.Disconnect(context.Background())
+
+	// Utwórz nowy serwer zamówień z podłączoną bazą danych MongoDB.
+	server := orders.NewServer(db)
+
+	// Wstaw przykładowe dane do bazy danych.
+	_, err = db.Database("db_ecommerce_mongo").Collection("orders").InsertOne(context.Background(), bson.M{
+		"id":     "order123",
+		"userId": "user123",
+		"productsId": []string{
+			"product1",
+			"product2",
+		},
+		"status": "created",
+	})
+	assert.NoError(t, err)
+
+	// Wywołaj funkcję GetOrder.
+	order, err := server.GetOrder(context.Background(), &orders.GetOrderRequest{Id: "order123"})
+	assert.NoError(t, err)
+	assert.NotNil(t, order)
+}
+
+func TestGetUserOrders(t *testing.T) {
+	// Utwórz nowego klienta MongoDB w pamięci.
+	db, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	assert.NoError(t, err)
+	defer db.Disconnect(context.Background())
+
+	// Utwórz nowy serwer zamówień z podłączoną bazą danych MongoDB.
+	server := orders.NewServer(db)
+
+	// Wstaw przykładowe dane do bazy danych.
+	_, err = db.Database("db_ecommerce_mongo").Collection("orders").InsertOne(context.Background(), bson.M{
+		"id":     "order123",
+		"userId": "user123",
+		"productsId": []string{
+			"product1",
+			"product2",
+		},
+		"status": "created",
+	})
+	assert.NoError(t, err)
+
+	// Wywołaj funkcję GetUserOrders.
+	ordersResponse, err := server.GetUserOrders(context.Background(), &orders.GetUserOrdersRequest{UserId: "user123"})
+	assert.NoError(t, err)
+	assert.NotNil(t, ordersResponse)
+	assert.Len(t, ordersResponse.Orders, 1)
+}
